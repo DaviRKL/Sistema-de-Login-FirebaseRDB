ea43d7f9a1b2324dc341023d622b97fec991c1e3
Davi Konuma <95696184+DaviRKL@users.noreply.github.com>
2024-05-27 11:06:33 -0300

    Update header.php

--- RDB-login/header.php
+++ RDB-login/header.php
@@ -11,7 +11,7 @@
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-<title> Fórum - Laravel </title>
+<title> Login - FirebaseRDB e PHP </title>
 </head>
 
 <body>
